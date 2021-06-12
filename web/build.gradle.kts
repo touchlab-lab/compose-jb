36dc2f2fd53696b4ae3bdbb82434af6476df11da
Kevin Galligan <kgalligan@gmail.com>
2021-06-11 20:47:46 -0400

    Save alternate plugins

--- web/build.gradle.kts
+++ web/build.gradle.kts
@@ -4,6 +4,24 @@ val COMPOSE_REPO_KEY: String? by project
 val COMPOSE_WEB_BUILD_WITH_EXAMPLES = project.property("COMPOSE_WEB_BUILD_WITH_EXAMPLES")!!.toString()?.toBoolean()
 
 apply<jetbrains.compose.web.gradle.SeleniumDriverPlugin>()
+buildscript {
+    dependencies {
+//        classpath deps.kotlin.gradlePlugin
+//        classpath "app.cash.exhaustive:exhaustive-gradle:${versions.exhaustive}"
+//        classpath 'com.vanniktech:gradle-maven-publish-plugin:0.14.2'
+//        classpath 'org.jetbrains.dokka:dokka-gradle-plugin:1.4.30'
+//        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.10")
+        classpath("com.github.jengelman.gradle.plugins:shadow:6.1.0")
+        classpath("com.android.tools.build:gradle:4.2.0")
+//        classpath 'com.diffplug.spotless:spotless-plugin-gradle:5.8.2'
+    }
+    repositories {
+        mavenLocal()
+        mavenCentral()
+        google()
+        jcenter()
+    }
+}
 
 subprojects {
     apply(plugin = "maven-publish")
@@ -42,6 +60,7 @@ subprojects {
 
     repositories {
         gradlePluginPortal()
+        mavenLocal()
         mavenCentral()
         maven { 
             url = uri("https://maven.pkg.jetbrains.space/public/p/compose/dev") 
